a6656288c99da38a18fa147b210672d4726e386b
kky <2910144663@qq.com>
2019-02-22 17:38:45 +0800

    首页登录

---
+++ GKHelpOutApp/GKHelpOutApp/Modules/Login/Controller/UploadAvatarViewController.h
@@ -0,0 +1,17 @@
+//
+//  UploadAvatarViewController.h
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/2/22.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UploadAvatarViewController : RootViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GKHelpOutApp/GKHelpOutApp/Modules/Login/Controller/UploadAvatarViewController.m
@@ -0,0 +1,13 @@
+//
+//  UploadAvatarViewController.m
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/2/22.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "UploadAvatarViewController.h"
+
+@implementation UploadAvatarViewController
+
+@end
