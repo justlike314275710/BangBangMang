793b75d27aa8e2871cf77a380b03d461d0673f69
kky <2910144663@qq.com>
2019-03-07 14:34:51 +0800

    咨询

---
+++ GKHelpOutApp/GKHelpOutApp/Modules/Mine/账单/Logic/HMBillLogic.h
@@ -0,0 +1,17 @@
+//
+//  HMBillLogin.h
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/3/7.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "HpBaseLogic.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMBillLogin : HpBaseLogic
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GKHelpOutApp/GKHelpOutApp/Modules/Mine/账单/Logic/HMBillLogic.m
@@ -0,0 +1,13 @@
+//
+//  HMBillLogin.m
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/3/7.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "HMBillLogin.h"
+
+@implementation HMBillLogin
+
+@end
