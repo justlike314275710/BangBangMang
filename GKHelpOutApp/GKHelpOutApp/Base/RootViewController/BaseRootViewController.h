da304191962b30a6eeb543b14d40607bf491f001
kky <2910144663@qq.com>
2019-04-19 09:14:10 +0800

    朋友圈

---
+++ GKHelpOutApp/GKHelpOutApp/Base/RootViewController/BaseRootViewController.h
@@ -0,0 +1,17 @@
+//
+//  BaseRootViewController.h
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/4/18.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "ZZFlexibleLayoutViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseRootViewController : ZZFlexibleLayoutViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GKHelpOutApp/GKHelpOutApp/Base/RootViewController/BaseRootViewController.m
@@ -0,0 +1,32 @@
+//
+//  BaseRootViewController.m
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/4/18.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "BaseRootViewController.h"
+
+@interface BaseRootViewController ()
+
+@end
+
+@implementation BaseRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
