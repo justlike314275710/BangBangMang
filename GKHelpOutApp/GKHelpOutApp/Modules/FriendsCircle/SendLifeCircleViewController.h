d5eb8e27eda99d8ffd3d20c0e7fca54752d46f82
kky <2910144663@qq.com>
2019-04-19 10:15:57 +0800

    fix bug

---
+++ GKHelpOutApp/GKHelpOutApp/Modules/FriendsCircle/SendLifeCircleViewController.h
@@ -0,0 +1,17 @@
+//
+//  SendLifeCircleViewController.h
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/4/19.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SendLifeCircleViewController : RootViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
