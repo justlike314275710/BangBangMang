a4a3ce760d024d3838f3fb5b3122170baeec14fc
kky <2910144663@qq.com>
2019-04-23 10:45:01 +0800

    盆友圈详情

---
+++ GKHelpOutApp/GKHelpOutApp/Modules/FriendsCircle/DetailLifeCircleViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailLifeCircleViewController.h
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/4/23.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailLifeCircleViewController : RootViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GKHelpOutApp/GKHelpOutApp/Modules/FriendsCircle/DetailLifeCircleViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailLifeCircleViewController.m
+//  GKHelpOutApp
+//
+//  Created by kky on 2019/4/23.
+//  Copyright © 2019年 kky. All rights reserved.
+//
+
+#import "DetailLifeCircleViewController.h"
+
+@interface DetailLifeCircleViewController ()
+
+@end
+
+@implementation DetailLifeCircleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
